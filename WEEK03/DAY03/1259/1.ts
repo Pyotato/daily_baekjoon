45727170b276a550e201ff5e4180b35cefb49660
Pyotato <pyolovely01@gmail.com>
2023-06-29 22:51:25 +0900

    1259 ts 풀이 완료

--- WEEK03/DAY03/1259/1.ts
+++ WEEK03/DAY03/1259/1.ts
@@ -1,1 +1,24 @@
-{}
+{
+  const error_msg = () => {
+    console.log("입력값이 부적절합니다.");
+  };
+  while (true) {
+    const num: string | null = prompt("숫자를 입력해주세요.");
+    if (num) {
+      if (parseInt(num) === 0) {
+        break;
+      }
+      const temp: Array<string> = [];
+      for (let i of num) {
+        temp.push(i);
+      }
+      temp.reverse();
+      const cmp: string = temp.join("");
+      cmp == num ? console.log("yes") : console.log("no");
+      continue;
+    } else {
+      error_msg();
+      break;
+    }
+  }
+}
