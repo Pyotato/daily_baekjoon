ffc7c4e7b14d7c064d86d419e9366df5f6339d83
Pyotato <pyolovely01@gmail.com>
2023-06-30 14:10:29 +0900

    fix prompt msg

--- WEEK08/DAY01/11720/1.ts
+++ WEEK08/DAY01/11720/1.ts
@@ -5,9 +5,7 @@
   const N: string | null = prompt(
     "1보다 크거나 같고 100보다 작거나 같은 값을 입력해주세요."
   );
-  const nums: string | null = prompt(
-    "1보다 크거나 같고 100보다 작거나 같은 값을 입력해주세요."
-  );
+  const nums: string | null = prompt(`${N}개의 숫자를 공백없이 입력해주세요.`);
   if (N) {
     let n: number = typeof parseInt(N) === "number" ? parseInt(N) : -1;
     let num_arr: Array<number> | null = nums
