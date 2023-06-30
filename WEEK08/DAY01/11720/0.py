c3ea5ad879060e84833cd9472dedc125e556164b
Pyotato <pyolovely01@gmail.com>
2023-06-30 10:53:52 +0900

    11720 ts python풀이 완료

---
+++ WEEK08/DAY01/11720/0.py
@@ -0,0 +1,4 @@
+from sys import stdin
+N = int(stdin.readline().strip())
+a = list(map(lambda i: int(i), stdin.readline().strip()))
+print(sum(a))
---
+++ WEEK08/DAY01/11720/1.ts
@@ -0,0 +1,24 @@
+{
+  const error_msg = () => {
+    console.log("입력값이 부적절합니다.");
+  };
+  const N: string | null = prompt(
+    "1보다 크거나 같고 100보다 작거나 같은 값을 입력해주세요."
+  );
+  const nums: string | null = prompt(
+    "1보다 크거나 같고 100보다 작거나 같은 값을 입력해주세요."
+  );
+  if (N) {
+    let n: number = typeof parseInt(N) === "number" ? parseInt(N) : -1;
+    let num_arr: Array<number> | null = nums
+      ? [...nums].map((v) => parseInt(v))
+      : null;
+    if (n && num_arr) {
+      console.log(num_arr.reduce((prv, nxt) => prv + nxt, 0));
+    } else {
+      error_msg();
+    }
+  } else {
+    error_msg();
+  }
+}
